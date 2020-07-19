28973fdd663c8245dca596ab61e32a7d0a4c7dc7
maxymove <sumeth.sir@student.mahidol.edu>
2020-07-19 23:56:56 +0700

    first commit

---
+++ backend/src/main/java/app/tutorfinder/TutorFinderApplication.java
@@ -0,0 +1,13 @@
+package app.tutorfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorFinderApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/app/tutorfinder/TutorFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package app.tutorfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
