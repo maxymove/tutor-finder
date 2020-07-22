9e69b9eb180bc26fbc7a47e77d6857ccc7d8b46a
Chakeera <chakeera1999@gmail.com>
2020-07-22 17:34:36 +0700

    frontend

--- frontend/src/service/CourseDataService.js
+++ frontend/src/service/CourseDataService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const INSTRUCTOR = "in28minutes";
+const INSTRUCTOR = "in28minutes";// admin
 const COURSE_API_URL = "http://localhost:9090";
 const INSTRUCTOR_API_URL = `${COURSE_API_URL}/instructors/${INSTRUCTOR}`;
 
@@ -8,6 +8,10 @@ class CourseDataService {
   retrieveAllCourses() {
     return axios.get(`${INSTRUCTOR_API_URL}/courses`);
   }
+  deleteCourse(name, id) {
+      return axios.delete(`${INSTRUCTOR_API_URL}/courses/${id}`);
+  }
+
 }
 
 export default new CourseDataService();
