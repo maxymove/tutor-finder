5f9641c9aab720dba2a2aa5e16ae0aaaf4896c36
Chakeera <chakeera1999@gmail.com>
2020-07-25 03:27:20 +0700

    add backend try

--- backend/src/main/java/app/tutorfinder/LoginController.java
+++ backend/src/main/java/app/tutorfinder/LoginController.java
@@ -1,10 +1,24 @@
-package app.tutorfinder.Controller;
+package app.tutorfinder;
 
+import app.tutorfinder.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = { "http://localhost:8080"}) // port of vue
 @RestController
 public class LoginController {
+    @Autowired
+    private CoursesHardCodedService courseManagementService;
+
+    @GetMapping("/login/{username}")
+    public List<Course> getAllCourses(@PathVariable String username) {
+        return courseManagementService.findAll();
+    }
+
 
 }
